7c7a01f53ae52da92a085356420b4d7f5abf6302
Tom Tzook <tomtzook@gmail.com>
2025-04-26 22:39:54 +0300

    Initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,87 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+
+    @Override
+    public void robotInit() {
+
+    }
+
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+
+    @Override
+    public void simulationInit() {
+
+    }
+
+    @Override
+    public void simulationPeriodic() {
+
+    }
+
+    @Override
+    public void disabledInit() {
+
+    }
+
+    @Override
+    public void disabledPeriodic() {
+
+    }
+
+    @Override
+    public void disabledExit() {
+
+    }
+
+    @Override
+    public void teleopInit() {
+
+    }
+
+    @Override
+    public void teleopPeriodic() {
+
+    }
+
+    @Override
+    public void teleopExit() {
+
+    }
+
+    @Override
+    public void autonomousInit() {
+
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+
+    }
+
+    @Override
+    public void autonomousExit() {
+
+    }
+
+    @Override
+    public void testInit() {
+
+    }
+
+    @Override
+    public void testPeriodic() {
+
+    }
+
+    @Override
+    public void testExit() {
+
+    }
+}
---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class RobotMap {
+
+    private RobotMap() {}
+
+    // add constants here
+    // public static final type NAME = value;
+}
