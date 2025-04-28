98c963a0d884b4305cedd73a2205a31c7c2378c0
Tom Tzook <tomtzook@gmail.com>
2025-04-28 22:28:25 +0300

    cleaned some leftovers

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,66 +1,21 @@
 package frc.robot;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.sim.Sim;
-import frc.robot.subsystems.DriveSystem;
 
 public class Robot extends TimedRobot {
-
-    // todo: start robot at an unknown position on the field each time
-    //      have to find real position with an april tag
-    //      and go to a specific position in the field (maybe to an april tag?)
-
-    private DriveSystem driveSystem;
-    private GenericHID controller;
-
-    private AprilTagFieldLayout fieldLayout;
-    private Field2d field2d = new Field2d();
 
     @Override
     public void robotInit() {
         //noinspection ResultOfMethodCallIgnored
         Sim.getInstance(); // will initialize sim stuff
-
-        driveSystem = new DriveSystem();
-        controller = new GenericHID(0);
-
-        fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
-        fieldLayout.setOrigin(AprilTagFieldLayout.OriginPosition.kBlueAllianceWallRightSide);
-        SmartDashboard.putData("field22222", field2d);
     }
-
 
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
         Sim.getInstance().update();
-
-        NetworkTable table = NetworkTableInstance.getDefault().getTable(RobotMap.LIMELIGHT_NAME);
-        boolean tv = table.getEntry("tv").getBoolean(false);
-        if (tv) {
-            int id = table.getEntry("tid").getNumber(0).intValue();
-            Number[] arr = table.getEntry("targetpose_camspace").getNumberArray(new Number[0]);
-            Transform3d llToTag = new Transform3d(arr[0].doubleValue(), arr[1].doubleValue(), arr[2].doubleValue(), new Rotation3d(arr[3].doubleValue(), arr[4].doubleValue(), arr[5].doubleValue()));
-
-            Pose3d aprilTagPose = fieldLayout.getTagPose(id).orElseThrow();
-            Pose3d llPose = aprilTagPose.plus(llToTag.inverse());
-            Pose3d robotPose = llPose.plus(RobotMap.LIMELIGHT_OFFSET_FROM_ROBOT_CENTER.inverse());
-
-            field2d.setRobotPose(robotPose.toPose2d());
-            field2d.getObject("apriltag").setPose(aprilTagPose.toPose2d());
-            field2d.getObject("ll").setPose(llPose.toPose2d());
-        }
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/DriveSystem.java
+++ src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -1,50 +1,8 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.configs.Pigeon2Configuration;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.NeutralOut;
-import com.ctre.phoenix6.controls.PositionDutyCycle;
-import com.ctre.phoenix6.hardware.Pigeon2;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
-import com.ctre.phoenix6.signals.InvertedValue;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.units.Units;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotMap;
-import frc.robot.sim.SimSystems;
 
 public class DriveSystem extends SubsystemBase {
-
-    private final TalonFX frontLeft;
-    private final TalonFX backLeft;
-    private final TalonFX frontRight;
-    private final TalonFX backRight;
-    private final Pigeon2 pigeon;
-
-    private final DifferentialDriveOdometry odometry;
-    private final Field2d field;
-
-    private final DutyCycleOut dutyCycleOutL = new DutyCycleOut(0);
-    private final DutyCycleOut dutyCycleOutR = new DutyCycleOut(0);
-    private final NeutralOut neutralOut = new NeutralOut();
-
-    // todo:
-    //      note: can't use the same control obj for two different requests in the same loop
-    //      note: setting sensor values is not supported
-    //      note: supported modes: NeutralOut, DutyCycleOut, PositionDutyCycle, Follower
-    //      work: formalize exercise
-    //      work: limelight sim. need to use perspective matrix to check what it sees
-    //      work: dont divide task too much, let talya come up with it together we me on zoom, to practice breaking up tasks
-    //              do task analysis together.
-    //              most doc should be on how things work and what is required
-    //      note: when docing ex, make sure to explain how to show poses on field
 
     public DriveSystem() {
         // TODO: UNCOMMENT
